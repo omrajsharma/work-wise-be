6ac987080a9f06d6b451d788e8706c1664c9cf56
omrajsharma <omraj7413@gmail.com>
2023-08-13 10:16:10 +0530

    feat: user login

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -1,5 +1,6 @@
 const UserModel = require('../model/UserModel');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const salt = bcrypt.genSaltSync(10);
 const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -55,4 +56,44 @@ const registerUser = async (req, res) => {
     }
 }
 
-module.exports = { registerUser };
+const loginUser = async (req, res) => {
+    const {username, password} = req.body;
+
+    if (!usernameRegex.test(username)) {
+        res.status(400).json({error: 'Invaid username! It should have only a-z A-Z 0-9 _ characters and should have 8-30 characters'})
+        return
+    }
+    if (!passwordRegex.test(password)) {
+        res.status(400).json({error: 'Invalid Password! Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character:'})
+        return
+    }
+
+    try {
+        const userDoc = await UserModel.findOne({username: username});
+        if (!userDoc){
+            res.status(400).json({error: 'Username Does not Exists'});
+            return;
+        }
+        const isPasswordCorrect = await bcrypt.compare(password, userDoc.password);
+        if (!isPasswordCorrect) {
+            res.status(400).json({error: 'Invalid Password'});
+            return;
+        }
+        
+        const token = jwt.sign({id : userDoc._id}, process.env.JWT_SECRET_KEY, {expiresIn: '1h'});
+        
+        res.cookie('token', token, {httpOnly: true, sameSite: 'none', secure: true})
+            .status(200)
+            .json({
+                success: 'User Logged In',
+                data: {
+                    userId: userDoc._id,
+                    username: userDoc.username
+                }
+            })
+    } catch (err) {
+        res.status(400).json({error: "something went wrong"})
+    }
+}
+
+module.exports = { registerUser, loginUser };
